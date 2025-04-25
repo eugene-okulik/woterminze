34f98b8c0f2e808514244a074d5aebb27e35ef3c
woterminze <qweaspot@gmail.com>
2025-04-25 22:20:47 +0300

    homework_5 done

---
+++ homework/tatyana_koptashkina/homework_5/hw5_task3.py
@@ -0,0 +1,5 @@
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+
+my_text = f"Students {', '.join(students)} study these subjects: {', '.join(subjects)}"
+print(my_text)
