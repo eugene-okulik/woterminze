05a9f1a6714c548f061f45f0c59d0a5b1f3a5939
woterminze <qweaspot@gmail.com>
2025-04-27 18:54:18 +0300

    hw 6 done

---
+++ homework/tatyana_koptashkina/homework_6/hw6_task1.py
@@ -0,0 +1,12 @@
+text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel.'
+       ' Integer urna nisl, facilisis vitae semper at, dignissim vitae libero')
+words = text.split()
+new_words =[]
+for word in words:
+    if word[-1] in {',', '.'}:
+        new_word = word[:-1] + 'ing' + word[-1]
+    else:
+        new_word = word + 'ing'
+    new_words.append(new_word)
+
+print(' '.join(new_words))
