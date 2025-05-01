8b3c9b3c5126c20c16f869cea0a9e46b90639c5d
woterminze <qweaspot@gmail.com>
2025-05-01 19:07:05 +0300

    fix lint

--- homework/tatyana_koptashkina/homework_7/hw7_task2.py
+++ homework/tatyana_koptashkina/homework_7/hw7_task2.py
@@ -1,11 +1,12 @@
-# или через while
-#items = list(words.items())
+words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
+
+# Вариант с while
+# items = list(words.items())
 # i = 0
-#while i < len(items):
+# while i < len(items):
 #     key, value = items[i]
 #     print(key * value)
 #     i += 1
-words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
 
 for key in words:
     value = words[key]
