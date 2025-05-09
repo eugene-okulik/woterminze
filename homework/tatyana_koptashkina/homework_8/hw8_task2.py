e197a7c03ce1aaa12d9a033ba90d1c92b75c4c8c
woterminze <qweaspot@gmail.com>
2025-05-09 20:45:16 +0300

    homework 8 done

---
+++ homework/tatyana_koptashkina/homework_8/hw8_task2.py
@@ -0,0 +1,19 @@
+def fibonacci(limit=100002):
+    a, b = 0, 1
+    count = 1
+    while count < limit:
+        yield a
+        a, b = b, a + b
+        count += 1
+
+fib_index = 1
+for a in fibonacci(100002):
+    if fib_index == 6:
+        print(a)
+    if fib_index == 201:
+        print(a)
+    #if fib_index == 1001:
+        #print(a)
+    #if fib_index == 100001:
+        #print(a)
+    fib_index += 1
