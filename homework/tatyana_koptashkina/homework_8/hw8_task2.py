71aecf9a80e4d81d269802f1067151535ed96f1e
woterminze <qweaspot@gmail.com>
2025-05-11 10:48:38 +0300

    fix limits

--- homework/tatyana_koptashkina/homework_8/hw8_task2.py
+++ homework/tatyana_koptashkina/homework_8/hw8_task2.py
@@ -1,20 +1,20 @@
-def fibonacci(limit=100002):
+def fibonacci():
     a, b = 0, 1
     count = 1
-    while count < limit:
+    while True:
         yield a
         a, b = b, a + b
         count += 1
 
 
 fib_index = 1
-for a in fibonacci(100002):
+for a in fibonacci():
     if fib_index == 6:
         print(a)
     if fib_index == 201:
         print(a)
-    # if fib_index == 1001:
-        # print(a)
-    # if fib_index == 100001:
-        # print(a)
+    if fib_index == 1001:
+        print(a)
+    if fib_index == 100001:
+        print(a)
     fib_index += 1
